093a6a457cc3d838ae3501aae5f54784a16ffb27
Gandhar Nerurkar <91309893+GandharNerurkar@users.noreply.github.com>
2023-03-11 23:33:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+function callvalue(){
+    var name = document.getElementById('uname');
+    var mobile = document.getElementById("mob");
+    var email = document.getElementById("email");
+    var eventn = document.getElementById("event");
+    document.writeln("Display Information: <br>"+"Your name: "+n+"<br>");
+    document.writeln("Your email: "+email);
+    document.writeln("Your mobile: "+mobile);
+}
+
